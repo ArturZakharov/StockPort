3db4ca565c8d74a4b377800c46e5fdea39a6180d
ZArthur88 <z.artur1988@gmail.com>
2021-01-05 20:35:15 +0200

    Initial Commit

---
+++ StockPort/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockPort
+//
+//  Created by ArturZaharov on 03.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
