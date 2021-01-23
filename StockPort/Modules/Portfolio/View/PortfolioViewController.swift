bb3e253e22f7fe6ead162cbec645c1b7805fc73b
ZArthur88 <z.artur1988@gmail.com>
2021-01-23 13:54:58 +0200

    added properties and funds to get currency

--- StockPort/Modules/Portfolio/View/PortfolioViewController.swift
+++ StockPort/Modules/Portfolio/View/PortfolioViewController.swift
@@ -13,9 +13,23 @@ class PortfolioViewController: UIViewController {
     @IBOutlet weak var currencyTableView: UITableView!
     @IBOutlet weak var userStocksTableView: UITableView!
     
+    //MARK:- Properties
+    let ser = CurrencyService.shared
+    var currencyValueArray: CurrencyValue? {
+        didSet{ getCurrency() }
+    }
+    var currencyValueElement: CurrencyData?
+    var currency: [Currency]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        ser.getCurrencyValue { value in
+            self.currencyValueArray = value
+        }
+        
+       
     }
 }
 
@@ -24,7 +38,7 @@ extension PortfolioViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch tableView {
         case currencyTableView:
-            return 1
+            return currency?.count ?? 0
         case userStocksTableView:
             return 1
         default:
@@ -44,5 +58,18 @@ extension PortfolioViewController: UITableViewDataSource{
         }
     }
     
-    
+    //func that get symbols for currency
+    func getCurrency(){
+        ser.getCurrencySimbols { valueElement in
+            self.currencyValueElement = valueElement
+            guard let currencyValueElement = self.currencyValueElement else {return}
+            guard let currencyValueArray = self.currencyValueArray else {return}
+            for (key, value) in currencyValueArray.currencyRates{
+                let currencyElement = Currency(name: key,
+                                        symbol: currencyValueElement.currency[key]?.symbol,
+                                        value: value)
+                self.currency?.append(currencyElement)
+            }
+        }
+    }
 }
