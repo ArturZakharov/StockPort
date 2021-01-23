d230fed2909c7f294f37826cc5510cc81e0ae3b5
ZArthur88 <z.artur1988@gmail.com>
2021-01-23 13:45:51 +0200

    created structs for currency value and symbols

---
+++ StockPort/Model/Currency.swift
@@ -0,0 +1,34 @@
+//
+//  CurrencyModel.swift
+//  StockPort
+//
+//  Created by ArturZaharov on 05.01.2021.
+//
+
+import Foundation
+
+//MARK:- struct to recive currency value
+struct CurrencyValue: Codable {
+    let currencyRates: [String: Double]
+
+    enum CodingKeys: String, CodingKey {
+        case currencyRates = "currency_rates"
+    }
+}
+
+//MARK:- structs to get currency symbols
+struct CurrencyData: Codable{
+    let currency: [String: CurrencyDetails]
+}
+
+struct CurrencyDetails: Codable {
+    let name: String
+    let symbol: String
+}
+
+//MARK:- final struct for using in app
+struct Currency {
+    let name: String
+    let symbol: String?
+    let value: Double
+}
