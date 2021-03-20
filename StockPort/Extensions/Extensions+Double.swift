65b79ac4fed7744321b7dcbba977d1da23d77926
ZArthur88 <z.artur1988@gmail.com>
2021-03-20 20:19:02 +0200

    extension for rounding Double was added

---
+++ StockPort/Extensions/Extensions+Double.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions+Double.swift
+//  StockPort
+//
+//  Created by ArturZaharov on 12.03.2021.
+//
+
+import Foundation
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+        
+    }
+}
